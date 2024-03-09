5f2b50828c022f1667bdb578b45636f0281e856b
Emile Maher <emile.maher@gmail.com>
2024-03-09 13:09:38 -0500

    not much

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 
 const int potPinX = 34;                // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6)
 const int potPinY = 35;                // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6)
+const int button1Pin = 32;             // Button 1 is connected to GPIO 32
+const int button2Pin = 33;             // Button 2 is connected to GPIO 33
+
+
 const int numberOfPotSamples = 5;     // Number of pot samples to take (to smooth the values)
 const int delayBetweenSamples = 5;    // Delay in milliseconds between pot samples
 const int delayBetweenHIDReports = 100; // Additional delay in milliseconds between HID reports
@@ -17,41 +21,12 @@ BleMouse bleMouse("La manette qui sent drôle", "Emile Maher", 69);
 void setup()
 {
     Serial.begin(9600);
-/*
-    // Create the BLE Device
-    BLEDevice::init("Joy-Livier");
-    // Create the BLE Server
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-    // Create the BLE Service
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    // Create a BLE Characteristic
-    pCharacteristic = pService->createCharacteristic(
-        CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ |
-            BLECharacteristic::PROPERTY_WRITE |
-            BLECharacteristic::PROPERTY_NOTIFY |
-            BLECharacteristic::PROPERTY_INDICATE);
-    // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-    // Create a BLE Descriptor
-    pCharacteristic->addDescriptor(new BLE2902());
-    // Start the service
-    pService->start();
-    // Start advertising
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
-    pAdvertising->setMinPreferred(0x12);
-    //BLEDevice::startAdvertising();
-    Serial.println("Waiting for a client connection to notify...");
-*/
     bleMouse.begin();
 }
 
 void loop()
 {
-    /*
+/*
     // notify changed value
     if (deviceConnected) {
         pCharacteristic->setValue((uint8_t *)&value, 4);
@@ -82,7 +57,7 @@ void loop()
     // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
     int offSet = 1000;
     int deadZone = 4000;
-    int reductionFactor = 1200;
+    int reductionFactor = 900;
     int maxValue = 32737;
 
     // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
@@ -101,36 +76,7 @@ void loop()
     moveY = moveY / reductionFactor;
 
     if (bleMouse.isConnected()) {
-      //  Serial.println("Scroll Down");
-        bleMouse.move(moveX, moveY, 0);
-      //  Serial.print("moveX: ");
-     //   Serial.print(moveX);
+        bleMouse.move(-moveX, -moveY, 0);
     }
-/*
-    delay(delayBetweenHIDReports);
 
-    // The code below (apart from the 2 closing braces) is for pot value degugging, and can be removed
-    // Print readings to serial port
-    Serial.print("Sent: ");
-    Serial.print(adjustedValueX);
-    Serial.print("\tRaw Avg: ");
-    Serial.print(potValue);
-    Serial.print("\tRaw: {");
-
-    // Iterate through raw pot values, printing them to the serial port
-    for (int i = 0; i < numberOfPotSamples; i++)
-    {
-        Serial.print(potValues[i]);
-
-        // Format the values into a comma seperated list
-        if (i == numberOfPotSamples - 1)
-        {
-            Serial.println("}");
-        }
-        else
-        {
-            Serial.print(", ");
-        }
-    }
-    */
 }
