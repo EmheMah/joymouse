6d4f3bfa7c2580b82a97ddd9d1d9cacdce0ba5f6
Emile Maher <emile.maher@gmail.com>
2024-06-15 15:43:48 -0400

    almost final, fine tuning

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <Math.h>
 
 #include <BleMouse.h>
 #include <BleConnectionStatus.h>
 
+#include <BleKeyboard.h>
+
+BleKeyboard bleKeyboard;
+BleMouse bleMouse("La manette qui sent drôle", "Emile Maher", 69);
+
+void handDrawNumberTwo();
+
 const int potPinX = 34; // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6)
 const int potPinY = 35; // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6)
 const int touchButtonPin = 4;
-const int buttonMode = 34;
+
 const int button1Pin = 32; // Button 1 is connected to GPIO 32
 const int button2Pin = 33; // Button 2 is connected to GPIO 33
 
 const int numberOfPotSamples = 5;        // Number of pot samples to take (to smooth the values)
 const int delayBetweenSamples = 5;       // Delay in milliseconds between pot samples
-const int delayBetweenHIDReports = 1000; // Additional delay in milliseconds between HID reports
-
-BleMouse bleMouse("La manette qui sent drôle", "Emile Maher", 69);
+const int delayBetweenHIDReports = 30; // Additional delay in milliseconds between HID reports
 
 // Declare the MODES array
 const String MODES[] = {F("MOUSE"), F("READING")};
@@ -36,72 +42,130 @@ void setup()
     bleMouse.begin();
 }
 
+void handDrawNumberTwo() {
+  int radius = 35;
+  int duration = 200;
+  
+  const int steps = 50;  // Number of steps to complete the circle
+  const float angleStep = 2 * PI / steps;  // Angle increment per step
+  const int delayPerStep = duration / steps;  // Delay between each step
+
+  for (int i = 0; i <= steps; i++) {
+    float angle = i * angleStep;
+    int x = static_cast<int>(radius * cos(angle));
+    int y = static_cast<int>(radius * sin(angle));
+    bleMouse.move(x, y);
+    delay(delayPerStep);
+  }
+}
+
 void loop()
 {
-    int potValuesX[numberOfPotSamples]; // Array to store pot readings
-    int potValueX = 0;                  // Variable to store calculated pot reading average
-    int potValuesY[numberOfPotSamples]; // Array to store pot readings
-    int potValueY = 0;                  // Variable to store calculated pot reading average
-
-    // Populate readings
-    for (int i = 0; i < numberOfPotSamples; i++)
-    {
-        potValuesX[i] = analogRead(potPinX);
-        potValueX += potValuesX[i];
-        potValuesY[i] = analogRead(potPinY);
-        potValueY += potValuesY[i];
-        delay(delayBetweenSamples);
-    }
-
-    // Calculate the average
-    // Calculate the average
-    potValueX = static_cast<int>(round(static_cast<double>(potValueX) / numberOfPotSamples));
-    potValueY = static_cast<int>(round(static_cast<double>(potValueY) / numberOfPotSamples));
-    // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
-    int offSet = 1000;
-    int deadZone = 4000;
-    int reductionFactor = 900;
-    int maxValue = 32737;
-
-    // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
-    int adjustedValueX = map(potValueX, 0, 4095, maxValue, 0) - offSet;
-    int moveX = int(maxValue / 2 - adjustedValueX);
-    if (abs(moveX) < deadZone)
-    {
-        moveX = 0;
-    }
-    moveX = moveX / reductionFactor;
-
-    int adjustedValueY = map(potValueY, 0, 4095, maxValue, 0) - offSet;
-    int moveY = int(maxValue / 2 - adjustedValueY);
-    if (abs(moveY) < deadZone)
-    {
-        moveY = 0;
-    }
-    moveY = moveY / reductionFactor;
-
 
     if (bleMouse.isConnected())
     {
-        Serial.println("connected");
-        // bleMouse.move(-moveX, -moveY, 0);
-        moveY = 1;
-        Serial.println("adjustedValueX : " + String(adjustedValueX) + " moveX : " + String(moveX));
-        Serial.println("adjustedValueY : " + String(adjustedValueY) + " moveY : " + String(moveY));
-        bleMouse.move(moveX, moveY, 0);
-        /*
-                if (touchRead(touchButtonPin) < 30) {
-                    bleMouse.press(MOUSE_LEFT);
-                    Serial.println("Left button pressed");
-                } else {
-                    bleMouse.release(MOUSE_LEFT);
-                }
+        int potValuesX[numberOfPotSamples]; // Array to store pot readings
+        int potValueX = 0;                  // Variable to store calculated pot reading average
+        int potValuesY[numberOfPotSamples]; // Array to store pot readings
+        int potValueY = 0;                  // Variable to store calculated pot reading average
 
-                if (analogRead(buttonMode) < 30) {
-                    Serial.println("Mode button pressed");
-                    currentModeIndex = getNextMode();
-                }
+        // Populate readings
+        for (int i = 0; i < numberOfPotSamples; i++)
+        {
+            potValuesX[i] = analogRead(potPinX);
+            potValueX += potValuesX[i];
+            potValuesY[i] = analogRead(potPinY);
+            potValueY += potValuesY[i];
+            delay(delayBetweenSamples);
+        }
+
+        // Calculate the average
+        // Calculate the average
+        potValueX = static_cast<int>(round(static_cast<double>(potValueX) / numberOfPotSamples));
+        potValueY = static_cast<int>(round(static_cast<double>(potValueY) / numberOfPotSamples));
+        // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
+        int offSet = 1000;
+        int deadZone = 3000;
+        int reductionFactor = 900;
+        int maxValue = 32737;
+
+        // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis reading
+        int adjustedValueX = map(potValueX, 0, 4095, maxValue, 0) - offSet;
+        int moveX = int(maxValue / 2 - adjustedValueX);
+        if (abs(moveX) < deadZone)
+        {
+            moveX = 0;
+        }
+        moveX = moveX / reductionFactor;
+
+        int adjustedValueY = map(potValueY, 0, 4095, maxValue, 0) - offSet;
+        int moveY = int(maxValue / 2 - adjustedValueY);
+        if (abs(moveY) < deadZone)
+        {
+            moveY = 0;
+        }
+        moveY = moveY / reductionFactor;
+
+        int button1Value = analogRead(button1Pin);
+        int button2Value = analogRead(button2Pin);
+        // Mode 0 : Stick moves cursor, B1 left click
+        // Mode 1 : Stick moves scroll/zoom, B1 right click
+
+        if (currentModeIndex == 0 ) {
+            bleMouse.move(-moveX, -moveY, 0);
+            if (analogRead(button1Pin) > 3000) {
+                Serial.println("MOUSE_LEFT");
+                bleMouse.click(MOUSE_LEFT);
+                delay(500);
+            }
+        } else if (currentModeIndex == 1 ) {
+            // Joystick
+            //               ScrollUp
+            // Back  ScrollLeft  ScrollRight Forward
+            //              ScrollDown
+/*
+            if (moveX <= -17) {
+                Serial.println("MOUSE_BACK");
+                bleMouse.click(MOUSE_BACK);
+                delay(500);
+            } if (moveX >= 17) {
+                Serial.println("MOUSE_FORWARD");
+                bleMouse.click(MOUSE_FORWARD);
+                delay(500);
+            } else {
+                bleMouse.move(0, 0, 0, round(-moveX / 10));
+            }
+
+            if (moveY != 0) {
+                bleMouse.move(0, 0, -moveY / abs(moveY));
+            }
+*/
+            bleMouse.move(0, 0, round(moveY / 7), round(moveX / 7));
+
+            if (analogRead(button1Pin) > 3000) {
+                Serial.println("MOUSE_RIGHT");
+                bleMouse.click(MOUSE_RIGHT);
+                delay(500);
+            }
+        }
+
+        // Button 2 changes mode
+        if (analogRead(button2Pin) > 3000) {
+            currentModeIndex = getNextMode();
+            Serial.println("Button 2 pressed : Mode set to " + String(currentModeIndex));
+            handDrawNumberTwo();handDrawNumberTwo();handDrawNumberTwo();
+        }
+
+        // Debug
+        if (moveX != 0 || moveY != 0) {
+            Serial.println("adjustedValueX : " + String(adjustedValueX) + " moveX : " + String(moveX));
+            Serial.println("adjustedValueY : " + String(adjustedValueY) + " moveY : " + String(moveY));
+        }
+        /*
+        Serial.println("button 1 : " + String(button1Value));
+        Serial.println("button 2 : " + String(button2Value));
         */
+
         delay(delayBetweenHIDReports); // bluetooth stack will go into congestion, if too many packets are sent
     }
 }
